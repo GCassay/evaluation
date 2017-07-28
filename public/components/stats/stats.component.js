d1b0e35d043b4880fd20043fad0de54b9c012e14
Gea <gea.cassinello@prodigious.com>
2017-07-28 15:16:26 -0500

    Chart improve

--- public/components/stats/stats.component.js
+++ public/components/stats/stats.component.js
@@ -8,7 +8,7 @@ angular .module('app')
 function statsController($http) {
 
   var vm = this;
-  vm.tab = 1;
+  vm.tab = 0;
 
   // Tab Controller
   vm.setTab = function(numTab) {
@@ -20,33 +20,34 @@ function statsController($http) {
   };
 
   // Get JSON Chart Data
-  (vm.translation = function() {
+  vm.chartData = function() {
 
     $http({
        method: 'GET',
        url: './data/stats.json'
     }).then(function (success){
 
-        vm.value = success.data.map( data => data.value );
-        vm.color = success.data.map( data => data.color );
-        vm.highlight = success.data.map( data => data.highlight );
-        vm.label = success.data.map( data => data.label );
+        vm.value = success.data[vm.tab].map( data => data.value );
+        vm.color = success.data[vm.tab].map( data => data.color );
+        vm.highlight = success.data[vm.tab].map( data => data.highlight );
+        vm.label = success.data[vm.tab].map( data => data.label );
 
         chartConfig(vm.value, vm.label, vm.color, vm.highlight);
     },function (error){
       console.log("Error en la carga de datos.");
     });
-  })();
+  }
 
   // Chart Config
   function chartConfig(value, label, color, highlight) {
-  var ctx = document.getElementById("myChart").getContext('2d');
-  var myChart = new Chart(ctx, {
+  var ctx = document.getElementById("diagram").getContext('2d');
+  if(vm.diagram){ vm.diagram.destroy(); }
+  vm.diagram = new Chart(ctx, {
       type: 'doughnut',
       data: {
           labels: label,
           datasets: [{
-              label: '# of Votes',
+              label: 'Usage',
               data: value,
               backgroundColor: color,
               highlightColor: highlight,
